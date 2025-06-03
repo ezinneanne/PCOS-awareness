cdb70316c7e9460c9ac55fa0b43f970dae67632e
Ezinne Anne Emilia <onwukaezinneanne@gmail.com>
2025-06-03 22:12:09 +0100

    first commit

--- PCOS-awareness/src/router/index.js
+++ PCOS-awareness/src/router/index.js
@@ -9,14 +9,21 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
-    /*{
+    {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
-    },*/
+    },
+    {
+      path: '/resources',
+      name: 'resources',
+      component: () => import('../views/ResourcesView.vue'),
+    },
+    {
+      path: '/pcosbot',
+      name: 'pcosbot',
+      component: () => import('../views/PCOSbotView.vue'),
+    },
   ],
 })
 
