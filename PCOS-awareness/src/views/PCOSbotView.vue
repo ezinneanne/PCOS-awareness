0c80a67413d263ec282bd7abf34776f4c9654adb
Ezinne Anne Emilia <onwukaezinneanne@gmail.com>
2025-06-07 19:22:36 +0100

    first commit

---
+++ PCOS-awareness/src/views/PCOSbotView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import NavBar from '../components/NavBar.vue';
+import TheFooter from '../components/TheFooter.vue';
+</script>
+
+<template>
+  <main>
+    <NavBar />
+    <TheFooter />
+  </main>
+</template>
