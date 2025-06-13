786e2b74585d3065255f1940894a467b94286944
Ezinne Anne Emilia <onwukaezinneanne@gmail.com>
2025-06-13 03:36:37 +0100

    first commit

--- backend/api/chat.py
+++ backend/api/chat.py
@@ -1,5 +1,11 @@
+import sys
+import os
 from fastapi import APIRouter, Request
 from pydantic import BaseModel
+
+# Add the backend folder to the system path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from scripts.rag_chain import query_rag
 
 router = APIRouter()
--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,10 @@
+import sys
+import os
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+# Add the backend folder to the system path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from api.chat import router as chat_router
 
 app = FastAPI()
